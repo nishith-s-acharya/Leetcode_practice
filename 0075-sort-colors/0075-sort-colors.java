633632ff665e6142ef6b632ed41ed8d49349b8e2
Nishith S Acharya <nishiths2704@gmail.com>
2026-07-28 20:18:40 +0530

    Time: 0 ms (100%), Space: 43.6 MB (43.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,35 +1,26 @@
 class Solution {
-    public void swap(int[] arr, int i ,int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] =temp;
-        
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-        for(int i =0;i<nums.length;i++){
-            System.out.print(nums[i]);
+    public void sortColors(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr,mid,low);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else if(arr[mid]==2){
+                swap(arr,mid,high);
+                high--;
+            }
         }
-        // int low = 0;
-        // int mid = 0;
-        // int high = nums.length-1;
-        // while(mid<=high){
-        //     if(nums[mid]==0){
-        //         swap(nums,low,mid);
-        //         low++;
-        //         mid++;
-        //     }
-        //     else if(nums[mid]==1){
-        //         mid++;
-        //     }else{
-        //         swap(nums,mid,high);
-        //         high--;
-              
-        //     }
-        // }
-        // for(int ele:nums){
-        //     System.out.print(ele+" ");
-        // }
-
     }
 }
