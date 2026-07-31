9d0f1cbbcbcb6b7549f7cca531ccb5b1069981b7
Nishith S Acharya <nishiths2704@gmail.com>
2026-07-31 16:16:43 +0530

    Time: 155 ms (5.1%), Space: 49.5 MB (5.17%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,59 +1,52 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-        int col0 = 1;
-        // this is for the rows and column outer
-        for(int i =0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-                if(arr[i][j]==0)
-                {
-                    arr[i][0] = 0;
+        
 
-                    if(j!=0){
-                        arr[0][j] = 0;
-                    }else{
-                        col0 = 0;
-                    }
+        //first check the element is zeror or not if yes mark it column and row marker to be 0
+        boolean colPart = false;
+        for(int i=0;i<arr.length;i++){
+            for(int j = 0;j<arr[i].length;j++){
+                if(arr[i][j]==0){
+                    arr[i][0] = 0;
                    
-                   
+
+                   if(j==0){
+                    colPart = true;
+                   }else{
+                    arr[0][j] = 0;
+                   }
+
                 }
             }
         }
-        // this is for the inner r&c
 
+        // after marking of i and j then check the row+1,col+1
         for(int i =1;i<arr.length;i++){
             for(int j=1;j<arr[i].length;j++){
-                if(arr[i][j]!=0){
-                    if(arr[0][j]==0 || arr[i][0]==0){
-                        arr[i][j] = 0;
-                    }
+                if(arr[0][j]==0 || arr[i][0]==0){
+                    arr[i][j] = 0;
                 }
             }
         }
 
-        // for the last outer loop
-        // change the column
-        if(arr[0][0] ==0 ){
-            for(int j = 0;j<arr[0].length;j++){
-                arr[0][j] = 0;
+        // check for the column
+
+        if(arr[0][0] == 0){
+            for(int i =0;i<arr[0].length;i++){
+                arr[0][i] = 0;
             }
         }
-        // change for the row
-        if(col0 == 0){
+        if(colPart){
             for(int i = 0;i<arr.length;i++){
                 arr[i][0] = 0;
             }
         }
 
-
-        for(int i =0;i<arr.length;i++){
-            for(int j = 0;j<arr[i].length;j++){
-                System.out.print(arr[i][j]+" ");
+        for(int i=0;i<arr.length;i++){
+            for(int j =0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]);
             }
             System.out.println();
         }
-
-
-
-       
     }
 }
