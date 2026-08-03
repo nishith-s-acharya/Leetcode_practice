d077149f56888cf0921b5e8b17e942cd9327576e
Nishith S Acharya <nishiths2704@gmail.com>
2026-08-04 00:16:21 +0530

    Time: 16 ms (21.53%), Space: 53.4 MB (7.5%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,51 +1,18 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int count1 = 0;
-        int count2 = 0;
-        int ele1 = Integer.MIN_VALUE;
-        int ele2 = Integer.MIN_VALUE;
-        
-        for(int i=0;i<nums.length;i++){
-            if(count1==0 && nums[i]!=ele2){
-                count1++;
-                ele1 = nums[i];
-               
-            }
-            else if(count2==0 && nums[i]!=ele1){
-                count2++;
-                ele2 = nums[i];
-            }
-            else if(nums[i]==ele1){
-                count1++;
-            }else if(nums[i]==ele2){
-                count2++;
-            } 
-            else{
-                count1--;
-                count2--;
-            }
-
-            
-        }
-        count1 = 0;
-        count2 = 0;
-        for(int num:nums){
-            if(num==ele1){
-                count1++;
-            }
-            else if(num ==ele2){
-                count2++;
-            }
-        }
-
-        ArrayList<Integer>list  = new ArrayList<>();
         int n = nums.length;
+        int county = n/3;
 
-        if(count1>(n/3)){
-            list.add(ele1);
+        // using the HashMap solving the problem
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int ele:nums){
+            map.put(ele,map.getOrDefault(ele,0)+1);
         }
-        if(count2>(n/3)){
-            list.add(ele2);
+        List<Integer> list = new ArrayList<>();
+        for(int ele:map.keySet()){
+            if(map.get(ele)>county){
+                list.add(ele);
+            }
         }
         return list;
     }
